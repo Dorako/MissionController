f2b5387258c7ccfef362d970fc444fd2ce92f142
Malthe Elkær <dirako@gmail.com>
2013-01-30 23:29:49 +0100

    Initial Commit

---
+++ MissionController/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MissionController
+//
+//  Created by Malthe Elkær on 1/30/13.
+//  Copyright (c) 2013 Malthe Elkær. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MissionController/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MissionController
+//
+//  Created by Malthe Elkær on 1/30/13.
+//  Copyright (c) 2013 Malthe Elkær. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
